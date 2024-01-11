eff6b2b9ee44ea49f9ad8f58fcfa92a36eceea17
reinhardtken <reinhardtken@hotmail.com>
2024-01-11 11:32:08 +0800

    update

---
+++ sdxxrm/ch04/test.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def test1():
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    ax = plt.axes(projection='3d')
+
+    # 三维线条的数据
+    zline = np.linspace(0, 15, 1000)
+    xline = np.sin(zline)
+    yline = np.cos(zline)
+    ax.plot3D(xline, yline, zline, 'gray')
+
+    # 三维散点的数据
+    zdata = 15 * np.random.random(100)
+    xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+    ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+    ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+    plt.show()
+
+
+def test2():
+    def f(x, y):
+        return np.sin(np.sqrt(x ** 2 + y ** 2))
+
+    x = np.linspace(-6, 6, 30)
+    y = np.linspace(-6, 6, 30)
+
+    X, Y = np.meshgrid(x, y)
+    Z = f(X, Y)
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.contour3D(X, Y, Z, 50, cmap='binary')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    # test1()
+    test2()
